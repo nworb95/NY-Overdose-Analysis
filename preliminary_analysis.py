5f8ea824c6c8bac0c0e3a4fa16bb795939cb035e
nworb95 <nworb95@gmail.com>
2019-04-11 16:52:31 -0400

    Playing around with async because there's a whole lot of data...

--- preliminary_analysis.py
+++ preliminary_analysis.py
@@ -1,10 +1,41 @@
 import modin.pandas as pd
 import re
 from sodapy import Socrata
+import logging
+from time import time
+import asyncio
+import aiohttp
+
+format = "%(asctime)s: %(message)s"
+logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
+logger = logging.getLogger(__name__)
+
+
+def get_dataset_string(json_string):
+    """
+    Grabs dataset string from URL.
+    :param json_string:
+    :return: dataset_string
+    """
+    return re.search(r"resource/(.*).json", json_string).group(1)
+
+
+async def get_data(client, dataset_string, offset, limit):
+    """
+    :param client: aiohttp ClientSession
+    :param dataset_string: string identifier for dataset
+    :param offset:
+    :param limit:
+    :return: json
+    """
+    return client.get(dataset_string, limit=limit, offset=offset)
 
 
 def offset_data(json_string):
-    """Works around default 1000 limit for reading from Socrata jsons.
+    """
+    Works around default 1000 limit for reading from Socrata jsons.
+    :param json_string:
+    :return: final_data_frame
     """
     if "health.data.ny" not in json_string:
         client = Socrata("data.ny.gov", None)
@@ -12,13 +43,17 @@ def offset_data(json_string):
         client = Socrata("health.data.ny.gov", None)
     offset = 0
     limit = 1000
-    dataset = re.search(r"resource/(.*).json", json_string).group(1)
+    dataset = get_dataset_string(json_string)
     results = []
     while client.get(dataset, limit=limit, offset=offset):
         data = pd.DataFrame.from_records(
-            client.get(dataset, limit=limit, offset=offset)
+            get_data(client, dataset, offset, limit)
         )
         results.append(data)
         offset = +limit
 
     return pd.concat(results)
+
+
+def main():
+    ts = time()
