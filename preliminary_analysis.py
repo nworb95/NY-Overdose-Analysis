8d97c4087aa1e4fdfd0d5f9d301fdcca83a3b936
nworb95 <nworb95@gmail.com>
2019-04-11 16:07:21 -0400

    Added modin for future use and API limit workaround.

--- preliminary_analysis.py
+++ preliminary_analysis.py
@@ -1,4 +1,24 @@
-import pandas as pd
+import modin.pandas as pd
+import re
+from sodapy import Socrata
 
-overdose_data = pd.read_json("https://data.cdc.gov/resource/a3uk-kgrx.json")
-print(overdose_data.columns.tolist())
+
+def offset_data(json_string):
+    """Works around default 1000 limit for reading from Socrata jsons.
+    """
+    if "health.data.ny" not in json_string:
+        client = Socrata("data.ny.gov", None)
+    else:
+        client = Socrata("health.data.ny.gov", None)
+    offset = 0
+    limit = 1000
+    dataset = re.search(r"resource/(.*).json", json_string).group(1)
+    results = []
+    while client.get(dataset, limit=limit, offset=offset):
+        data = pd.DataFrame.from_records(
+            client.get(dataset, limit=limit, offset=offset)
+        )
+        results.append(data)
+        offset = +limit
+
+    return pd.concat(results)
