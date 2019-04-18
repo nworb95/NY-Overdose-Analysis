9553c23b18b1be57609bcc248682c2b246b31d2a
nworb95 <nworb95@gmail.com>
2019-04-18 11:52:41 -0400

    Refactoring functions a bit

--- preliminary_analysis.py
+++ preliminary_analysis.py
@@ -2,11 +2,22 @@ import modin.pandas as pd
 import re
 from sodapy import Socrata
 import logging
+from sqlalchemy import create_engine
 import os
 
 format = "%(asctime)s: %(message)s"
 logging.basicConfig(format=format, level=logging.INFO, datefmt="%H:%M:%S")
 logger = logging.getLogger(__name__)
+
+
+def connect_to_mysql():
+    """
+    Connects to local mysql database to cache downloaded data.
+    :return: engine
+    """
+    return create_engine(
+        "mysql://root:{}@localhost".format(os.environ["MYSQL_LOCAL_PASS"])
+    )
 
 
 def get_dataset_string(json_string):
@@ -29,33 +40,34 @@ def get_data(client, dataset_string, offset, limit):
     return client.get(dataset_string, limit=limit, offset=offset)
 
 
-def offset_data(json_string):
+def download_data(client, table):
+    offset = 0
+    limit = 50000
+    results = []
+    while client.get(table, limit=limit, offset=offset):
+        data = pd.DataFrame.from_records(get_data(client, table, offset, limit))
+        results.append(data)
+        offset += len(data)
+        logger.info("Downloaded %d rows!", offset)
+    return pd.concat(results)
+
+
+def load_data(json_string):
     """
     Works around default 1000 limit for reading from Socrata jsons.
     :param json_string:
-    :return: final_data_frame
+    :return: dictionary with dataframe as value and table title as key
     """
     if "health.data.ny" not in json_string:
-        client = Socrata("data.ny.gov", os.environ['SOCRATA_TOKEN'])
-        logger.info('Connected to NY Data Socrata client!')
+        client = Socrata("data.ny.gov", os.environ["SOCRATA_TOKEN"])
+        logger.info("Connected to NY Data Socrata client!")
     else:
-        client = Socrata("health.data.ny.gov", os.environ['SOCRATA_TOKEN'])
-        logger.info('Connected to NY Health Data Socrata client!')
-    offset = 0
-    limit = 50000
-    dataset = get_dataset_string(json_string)
-    results = []
-    while client.get(dataset, limit=limit, offset=offset):
-        data = pd.DataFrame.from_records(
-            get_data(client, dataset, offset, limit)
-        )
-        results.append(data)
-        offset += len(data)
-        logger.info('Downloaded %d rows!', offset)
-
-    return pd.concat(results)
+        client = Socrata("health.data.ny.gov", os.environ["SOCRATA_TOKEN"])
+        logger.info("Connected to NY Health Data Socrata client!")
+    table_string = get_dataset_string(json_string)
+    table_title = client.get_metadata(table_string)
+    return {table_title: download_data(client, table_string)}
 
 
 def main():
     ts = time()
-
