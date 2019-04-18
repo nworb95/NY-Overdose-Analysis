67ad4ac3d118604416ebbd926471052ece37ab33
nworb95 <nworb95@gmail.com>
2019-04-18 15:05:16 -0400

    Refactoring

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from database_utils import generate_database
+from database_utils.database_utils import generate_database
+import time
 
 
 __author__ = "Emma Brown"
 __version__ = "0.1.0"
 __license__ = "MIT"
-
-
 
 
 def main():
