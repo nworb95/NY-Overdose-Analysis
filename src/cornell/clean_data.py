817819d0c7d487bb356b859698c4f3affbc899f0
nworb95 <nworb95@gmail.com>
2020-01-20 16:03:39 -0500

    Added projected population data functions

--- src/cornell/clean_data.py
+++ src/cornell/clean_data.py
@@ -7,3 +7,17 @@ def clean_actual_population_data(df):
     )
     df["County"] = df["County"].str.replace(".", "")
     return df[~(df["County"] == "New York")]
+
+
+def clean_projected_population_data(df):
+    filtered_df = df[((df["SEX_DESCR"] == "All") & (df["AGEGRP_DESCR"] == "Total"))]
+    clean_df = filtered_df.rename(
+        columns={"COUNTY_DESCR": "County", "YR_2015": "2015", "YR_2016": "2016", "YR_2017": "2017", "YR_2018": "2018",
+                 "YR_2019": "2019", "YR_2020": "2020", "YR_2021": "2021", "YR_2022": "2022", "YR_2023": "2023",
+                 "YR_2024": "2024", "YR_2025": "2025", "YR_2026": "2026", "YR_2027": "2027", "YR_2028": "2028",
+                 "YR_2029": "2029", "YR_2030": "2030", "YR_2031": "2031", "YR_2032": "2032", "YR_2033": "2033",
+                 "YR_2034": "2034", "YR_2035": "2035", "YR_2036": "2036", "YR_2037": "2037", "YR_2038": "2038",
+                 "YR_2039": "2039", "YR_2040": "2040"}
+    ).drop(["COUNTY", "SEXCODE", "SEX_DESCR", "AGEGRPCODE", "AGEGRP_DESCR", "RACECODE", "RACE_DESCR"], axis=1)
+    clean_df['County'] = clean_df['County'] + ' County'
+    return clean_df
--- src/cornell/pull_data.py
+++ src/cornell/pull_data.py
@@ -1,6 +1,72 @@
 import requests
 import pandas as pd
 from io import BytesIO
+import xlrd
+
+county_list = [
+    1,
+    3,
+    5,
+    7,
+    9,
+    11,
+    13,
+    15,
+    17,
+    19,
+    21,
+    23,
+    25,
+    27,
+    29,
+    31,
+    33,
+    35,
+    37,
+    39,
+    41,
+    43,
+    45,
+    47,
+    49,
+    51,
+    53,
+    55,
+    57,
+    59,
+    61,
+    63,
+    65,
+    67,
+    69,
+    71,
+    73,
+    75,
+    77,
+    79,
+    81,
+    83,
+    87,
+    85,
+    89,
+    91,
+    93,
+    95,
+    97,
+    99,
+    101,
+    103,
+    105,
+    107,
+    109,
+    111,
+    113,
+    115,
+    117,
+    119,
+    121,
+    123,
+]
 
 
 def get_actual_population_data():
@@ -21,3 +87,31 @@ def cache_actual_population_data(df):
 def pull_actual_population_data():
     df = get_actual_population_data()
     cache_actual_population_data(df)
+    return df
+
+
+def get_projected_population_data(county_list):
+    projection_df_list = []
+    for request in county_list:
+        projection_df_list.append(
+            pd.read_excel(
+                BytesIO(
+                    requests.get(
+                        "https://pad.human.cornell.edu/counties/expprojdata.cfm?county={}".format(
+                            request
+                        )
+                    ).content
+                )
+            )
+        )
+    return pd.concat(projection_df_list)
+
+
+def cache_projected_population_data(df):
+    df.to_json('./raw_data/projected_ny_population_data.json')
+
+
+def pull_projected_population_data(county_list):
+    df = get_projected_population_data(county_list)
+    cache_projected_population_data(df)
+    return df
