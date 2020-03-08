751f6d78727bcebb1666ac9e925d81d7c2c0bead
nworb95 <nworb95@gmail.com>
2020-03-08 16:34:16 -0400

    Ripped out elastic-search-py, replaced with requests

--- src/elk/upload_data.py
+++ src/elk/upload_data.py
@@ -1,20 +1,21 @@
-import src.elk.utils as utils
+import json
+import requests
+
 import pandas as pd
-import hashlib, json, requests
-from elasticsearch import Elasticsearch, helpers
 
+import src.elk.utils as utils
 
-fpath = "../../data/raw_data/recidivism_data_by_county/0.json"
+file_path = "../../data/raw_data/recidivism_data_by_county/0.json"
 index = "recidivism_by_county"
-elk_url = "http://localhost:9200/{}".format(index)
-# data = utils.format_year_column(pd.read_json(fpath), "release_year")
+column = "release_year"
 
 
-def post_to_es(df, index=index, server=elk_url, chunk_size=2000):
+def post_to_es(df, index_name, chunk_size=2000):
     headers = {"content-type": "application/x-ndjson", "Accept-Charset": "UTF-8"}
+    server = "http://localhost:9200/{}".format(index_name)
     records = df.to_dict(orient="records")
     actions = [
-        """{ "index" : { "_index" : "%s"} }\n""" % (index) + json.dumps(records[j])
+        """{ "index" : { "_index" : "%s"} }\n""" % index_name + json.dumps(records[j])
         for j in range(len(records))
     ]
     i = 0
@@ -42,10 +43,6 @@ def doc_generator(df, index_name):
     raise StopIteration
 
 
-def upload_socrata_data(json_path, index):
-    es_client = Elasticsearch(http_compress=True)
-    data = utils.format_year_column(pd.read_json(json_path), "release_year")
-    doc = doc_generator(data, index)
-    # return helpers.bulk(es_client, doc_generator(data, index))
-    for i in doc:
-        print(i)
+def upload_socrata_data(json_path, index_name, time_column):
+    data = utils.format_year_column(pd.read_json(json_path), time_column)
+    post_to_es(data, index_name)
