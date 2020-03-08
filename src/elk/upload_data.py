b535623f74ea4ff8af06d6dcdb7f6c17caa626c7
nworb95 <nworb95@gmail.com>
2020-03-07 19:37:17 -0500

    Added upload_socrata_data function

--- src/elk/upload_data.py
+++ src/elk/upload_data.py
@@ -1,21 +1,22 @@
 import src.elk.utils as utils
 import pandas as pd
+import hashlib
 from elasticsearch import Elasticsearch, helpers
 
-es_client = Elasticsearch(http_compress=True)
-json = pd.read_json('../../data/mental_health_data_by_county/0.json')
 
-
-def doc_generator(df):
+def doc_generator(df, index_name, doc_name):
     df_gen = df.iterrows()
     for index, document in df_gen:
         yield {
-            "_index": 'your_index',
-            "_type": "_doc",
-            "_id": f"{document['id']}",
-            "_source": utils.filter_keys(document)
+            "_index": index_name,
+            "_doc": doc_name,  # or doc??
+            "_id": f"{hashlib.sha1(doc_name.encode()).hexdigest() + '_' + str(index)}",
+            "_source": utils.filter_keys(document.to_dict(), df.columns)
         }
     raise StopIteration
 
 
-helpers.bulk(es_client, doc_generator(json))
+def upload_socrata_data(json_path, index, document):
+    es_client = Elasticsearch(http_compress=True)
+    data = utils.format_year_column(pd.read_json(json_path), 'service_year')
+    return helpers.bulk(es_client, doc_generator(data, index, document))
--- src/elk/utils.py
+++ src/elk/utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 import pandas as pd
+import dateutil
 
 
 def safe_date(date_value):
@@ -15,3 +16,8 @@ def safe_value(field_val):
 
 def filter_keys(document, use_these_keys):
     return {key: document[key] for key in use_these_keys}
+
+
+def format_year_column(data, column_name):
+    data[column_name] = data[column_name].apply(lambda x: dateutil.parser.parse(str(x)))
+    return data
