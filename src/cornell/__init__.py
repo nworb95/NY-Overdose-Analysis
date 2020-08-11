854b42a595dda9b690acee8eb4b5e09903f3deab
Emma Brown <nworb95@gmail.com>
2020-08-10 21:16:35 -0400

    Black formatting:

--- src/cornell/__init__.py
+++ src/cornell/__init__.py
@@ -1,1 +1,4 @@
-from src.cornell.pull_population_data import pull_projected_population_data, pull_actual_population_data
+from src.cornell.pull_population_data import (
+    pull_projected_population_data,
+    pull_actual_population_data,
+)
--- src/cornell/clean_population_data.py
+++ src/cornell/clean_population_data.py
@@ -1,8 +1,12 @@
 import pandas as pd
 import logging
 
-from src.cornell.constants import CORNELL_PROJECTED_DROP_COLUMNS, CORNELL_HISTORICAL_POPULATION_COLUMNS, \
-    CORNELL_PROJECTED_POPULATION_COLUMNS, CORNELL_MISSING_POPULATION_YEARS
+from src.cornell.constants import (
+    CORNELL_PROJECTED_DROP_COLUMNS,
+    CORNELL_HISTORICAL_POPULATION_COLUMNS,
+    CORNELL_PROJECTED_POPULATION_COLUMNS,
+    CORNELL_MISSING_POPULATION_YEARS,
+)
 
 
 def clean_historical_population_data(df: pd.DataFrame):
@@ -15,9 +19,9 @@ def clean_historical_population_data(df: pd.DataFrame):
     Returns:
         pd.DataFrame: clean data
     """
-    df = df.rename(
-        columns=CORNELL_HISTORICAL_POPULATION_COLUMNS
-    ).drop(["Unnamed: 1", "Unnamed: 12"], axis=1)
+    df = df.rename(columns=CORNELL_HISTORICAL_POPULATION_COLUMNS).drop(
+        ["Unnamed: 1", "Unnamed: 12"], axis=1
+    )
     df["County"] = df["County"].str.replace(".", "")
     return df[~(df["County"] == "New York")]
 
@@ -33,11 +37,8 @@ def clean_projected_population_data(df: pd.DataFrame):
         pd.DataFrame: clean data
     """
     filtered_df = df[(df["SEX_DESCR"] == "All") & (df["AGEGRP_DESCR"] == "Total")]
-    clean_df = filtered_df.rename(
-        columns=CORNELL_PROJECTED_POPULATION_COLUMNS
-    ).drop(
-        CORNELL_PROJECTED_DROP_COLUMNS,
-        axis=1,
+    clean_df = filtered_df.rename(columns=CORNELL_PROJECTED_POPULATION_COLUMNS).drop(
+        CORNELL_PROJECTED_DROP_COLUMNS, axis=1,
     )
     clean_df["County"] = clean_df["County"] + " County"
     return clean_df.reset_index().drop(["index"], axis=1)
@@ -49,7 +50,9 @@ def format_population_data(merged_df: pd.DataFrame):
     :param merged_df:
     :return:
     """
-    import pdb; pdb.set_trace()
+    import pdb
+
+    pdb.set_trace()
     cols = [x for x in merged_df.columns.tolist() if x != "County"]
     for i in cols:
         merged_df[i] = merged_df[i].astype(int)
@@ -67,7 +70,9 @@ def merge_population_data(historical_df: pd.DataFrame, projected_df: pd.DataFram
         pd.DataFrame: clean population data
     """
     df = pd.merge(historical_df, projected_df, on="County")
-    big_df = pd.concat([df, pd.DataFrame(columns=CORNELL_MISSING_POPULATION_YEARS)], sort=False)
+    big_df = pd.concat(
+        [df, pd.DataFrame(columns=CORNELL_MISSING_POPULATION_YEARS)], sort=False
+    )
     for year in CORNELL_MISSING_POPULATION_YEARS:
         big_df[year] = pd.to_numeric(big_df[year], errors="coerce")
     interpolate_df = big_df.drop("County", axis=1)
