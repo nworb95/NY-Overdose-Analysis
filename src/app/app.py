cf402e01121a41dc66a11b07b078f6d834d8dbc1
Emma Brown <nworb95@gmail.com>
2020-10-30 22:09:18 -0400

    Added navbar

--- src/app/app.py
+++ src/app/app.py
@@ -10,3 +10,19 @@ from bokeh.transform import factor_cmap
 from flask import Flask, render_template, request
 
 from bokeh_plotter import p
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def chart():
+    script_county_chart, div_county_chart = components(p)
+    return render_template(
+        'index.html',
+        div_county_chart=div_county_chart,
+        script_county_chart=script_county_chart
+    )
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
