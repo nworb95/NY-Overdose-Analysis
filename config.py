00b45a6f004f1ca676f6c7237cd49bbfeacf1293
Emma Brown <nworb95@gmail.com>
2020-11-15 18:57:16 -0500

    Added config class according to Miguel Grinberg tutorial

---
+++ config.py
@@ -0,0 +1,9 @@
+import os
+
+base_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(base_dir, 'src/app/app.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
